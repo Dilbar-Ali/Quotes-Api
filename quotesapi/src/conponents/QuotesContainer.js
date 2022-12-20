9d6dded36a86bef58a9da93e9578c9bfc9346109
Dilbar-ali <dilbarali0013@gmail.com>
2022-12-20 23:50:21 +0500

    SuccesFull 90% work complete

--- quotesapi/src/conponents/QuotesContainer.js
+++ quotesapi/src/conponents/QuotesContainer.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import "./QuotesContainer.css"
 import Quote from './Quote'
 const Quotes_container = () => {
+
+  const [Quotes,setQuotes]=useState([]);
+
+  const getTenRandomQuotes=(Quotes)=>{
+    const randomQuotes=[];
+    for(let i=0;i<10;i++){
+      const randomIndex=Math.floor(Math.random()*Quotes.length);
+      randomQuotes.push(Quotes[randomIndex]);
+    }
+    return randomQuotes;
+  }
+
+  useEffect(() => {
+    const fetchQuotes=async()=>{
+      const response=await fetch("http://type.fit/api/quotes");
+   const data=await response.json();
+   const randomQuotes=getTenRandomQuotes(data);
+   console.log(data);
+   setQuotes(randomQuotes);
+      
+    }
+  
+    fetchQuotes();
+  }, [])
+  
+
   return (
     <div className="main-container">
     <h1>Best inspirational Quotes</h1>
@@ -12,6 +38,7 @@ const Quotes_container = () => {
     <p>They all are famous peple</p>
 
     <div className="quotes-container">
+     <Quote/>
      <Quote/>
     </div>
   </div>
